f13af084893a08cd037515497cc2c233db00deaf
Eduard Klimenko <edik.klimenko@gmail.com>
2022-12-21 14:27:12 +0200

    fix: save order state to localStorage

--- src/app/services/cartapi.service.ts
+++ src/app/services/cartapi.service.ts
@@ -2,22 +2,36 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+const OREDR: string = 'ORDER';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartapiService {
   cartDataList: any = [];
   productList = new BehaviorSubject<any>([]);
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.cartDataList = this.sGet(OREDR) || [];
+    this.productList.next(this.cartDataList);
+  }
+  sGet(id: string) {
+    return JSON.parse(localStorage.getItem(id) || 'null');
+  }
+  sSet(id: string, data: any){
+    localStorage.setItem(id, JSON.stringify(data));
+  }
   getProductData() {
     return this.productList.asObservable();
   }
   setProduct(product: any) {
     this.cartDataList.push(...product);
+    this.sSet(OREDR, this.cartDataList);
     this.productList.next(product);
   }
   addToCart(product: any) {
     this.cartDataList.push(product);
+    this.sSet(OREDR, this.cartDataList);
+
     this.productList.next(this.cartDataList);
     this.getTotalAmount();
     console.log(this.cartDataList)
@@ -35,11 +49,13 @@ export class CartapiService {
       if (product.id === a.id) {
         this.cartDataList.splice(index, 1)
       }
-    })
+    });
+    this.sSet(OREDR, this.cartDataList);
     this.productList.next(this.cartDataList);
   }
   removeAllCart(){
     this.cartDataList = [];
+    this.sSet(OREDR, this.cartDataList)
     this.productList.next(this.cartDataList)
   }
 }
