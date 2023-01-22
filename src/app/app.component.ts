2989b6c54ecf6bf0113e5f2e24a643e805418645
Danylo Samsonenko <d.samsonenko111@gmail.com>
2023-01-22 15:07:36 +0300

    add autoscroll to up new pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,29 @@ export class AppComponent {
     this.lang = new RegExp('\/ru\/').test(this.document.location.href) ? 'ru' : 'ua';
     translate.use(this.lang);
   }
+//   onActivate(event: any) {
+//     // window.scroll(0,0);
+ 
+//     window.scroll({ 
+//             top: 0, 
+//             left: 0, 
+//             behavior: 'smooth' 
+//      });
+ 
+//      //or document.body.scrollTop = 0;
+//      //or document.querySelector('body').scrollTo(0,0)
+     
+//  }
+onActivate(event: any) {
+  let scrollToTop = window.setInterval(() => {
+      let pos = window.pageYOffset;
+      if (pos > 0) {
+          window.scrollTo(0, pos - 20); // how far to scroll on each step
+      } else {
+          window.clearInterval(scrollToTop);
+      }
+  }, 16);
+}
 
   ngOnInit(): void {
     this.cartApi.getProductData().subscribe(res => {
